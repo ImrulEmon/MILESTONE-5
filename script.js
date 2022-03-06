65a611a1df0b6ff715cb0da99fe5cc9212e08ec6
ImrulEmon <emimrulemon@gmail.com>
2022-03-06 18:46:37 +0600

    total done

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
 function extraPrice(component,base,price) {
-    const extraComponentPriceField = document.getElementById("extra-"+component);
-   
     if(base==false){
-        extraComponentPriceField.innerText= price;
+        document.getElementById("extra-"+component).innerText= price;
     }
     
     else{
-        extraComponentPriceField.innerText = price;
+        document.getElementById("extra-"+component).innerText= price;
         
     }
 }
 
-function updateTotal(price,component) {
-    const previousTotal= document.getElementById("total").innerText;
+function updateTotal() {
+   // const previousTotal= document.getElementById("total").innerText;
+   const base = 1300 ;
     const extraMemory = document.getElementById("extra-memory").innerText;
     const extraStorage = document.getElementById("extra-storage").innerText;
-    const total = parseInt(previousTotal) + parseInt(extraMemory)+parseInt(extraStorage);
-    console.log(total);
+    const extraDelivery = document.getElementById("extra-delivery").innerText;
+    const total = parseInt(base) + parseInt(extraMemory)+parseInt(extraStorage)+parseInt(extraDelivery);
+    document.getElementById("total").innerText = total;
 }
-
 document.getElementById("8gb").addEventListener('click',function () {
     extraPrice("memory",true,0);
-    updateTotal(0,"memory");
+    updateTotal();
     
 });
 document.getElementById("16gb").addEventListener('click',function () {
-    extraPrice("memory",false,180);
-    updateTotal(0,"memory");
+    extraPrice("memory",false,200);
+    updateTotal();
 });
 
 
 document.getElementById("256gb").addEventListener('click',function () {
     extraPrice("storage",true,0);
-    updateTotal(0,"storage");
+    updateTotal();
 });
 document.getElementById("512gb").addEventListener('click',function () {
     extraPrice("storage",false,100);
-    updateTotal(100,"storage");
+    updateTotal();
 });
 document.getElementById("1tb").addEventListener('click',function () {
-    extraPrice("storage",false,180);
-    updateTotal(180,"storage");
+    extraPrice("storage",false,200);
+    updateTotal();
+});
+
+document.getElementById("free").addEventListener('click',function () {
+    extraPrice("delivery",true,0);
+    updateTotal();
+    
+});
+document.getElementById("charge").addEventListener('click',function () {
+    extraPrice("delivery",false,20);
+    updateTotal();
 });
