29c1b188e3a1866b2f3184eadf0b705caf84f7c6
ImrulEmon <emimrulemon@gmail.com>
2022-03-06 17:58:41 +0600

    total before delivary

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("script");
+function extraPrice(component,base,price) {
+    const extraComponentPriceField = document.getElementById("extra-"+component);
+   
+    if(base==false){
+        extraComponentPriceField.innerText= price;
+    }
+    
+    else{
+        extraComponentPriceField.innerText = price;
+        
+    }
+}
+
+function updateTotal(price,component) {
+    const previousTotal= document.getElementById("total").innerText;
+    const extraMemory = document.getElementById("extra-memory").innerText;
+    const extraStorage = document.getElementById("extra-storage").innerText;
+    const total = parseInt(previousTotal) + parseInt(extraMemory)+parseInt(extraStorage);
+    console.log(total);
+}
+
+document.getElementById("8gb").addEventListener('click',function () {
+    extraPrice("memory",true,0);
+    updateTotal(0,"memory");
+    
+});
+document.getElementById("16gb").addEventListener('click',function () {
+    extraPrice("memory",false,180);
+    updateTotal(0,"memory");
+});
+
+
+document.getElementById("256gb").addEventListener('click',function () {
+    extraPrice("storage",true,0);
+    updateTotal(0,"storage");
+});
+document.getElementById("512gb").addEventListener('click',function () {
+    extraPrice("storage",false,100);
+    updateTotal(100,"storage");
+});
+document.getElementById("1tb").addEventListener('click',function () {
+    extraPrice("storage",false,180);
+    updateTotal(180,"storage");
+});
